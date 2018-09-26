580f55a552d4f8f6df7848e708e94bd7738b108e
Dina Cordova <Dina Cordova@PCHOME1>
2018-09-26 05:25:25 -0600

    prototipo pagina busquedaProductos finalizada, continua en 5.2 00:00

---
+++ SistemaComercial/src/sv/gob/cnr/sistemacomercial/controller/BusquedaProductosMB.java
@@ -0,0 +1,28 @@
+package sv.gob.cnr.sistemacomercial.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="busquedaProductosMB")
+@RequestScoped
+public class BusquedaProductosMB {
+	private List<Integer> productosFiltrados;
+
+	public BusquedaProductosMB() {
+		productosFiltrados = new ArrayList<Integer>();
+		for(int i=0; i<50; i++){
+			productosFiltrados.add(i);
+		}
+	}
+
+	public List<Integer> getProductosFiltrados() {
+		return productosFiltrados;
+	}
+	
+	
+	
+	
+}
