c505c930835d31cb0a023abc3ec149b2536e5506
Dina Cordova <Dina Cordova@PCHOME1>
2018-10-05 05:47:07 -0600

    Se completo registroPedido, continua 6.1 00:00

---
+++ SistemaComercial/src/sv/gob/cnr/sistemacomercial/controller/RegistroPedidoBean.java
@@ -0,0 +1,23 @@
+package sv.gob.cnr.sistemacomercial.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="registroPedidoBean")
+@RequestScoped
+public class RegistroPedidoBean {
+	private List<Integer> items;
+
+	public RegistroPedidoBean() {
+		items = new ArrayList<Integer>();
+		items.add(1);
+	}
+
+	public List<Integer> getItems() {
+		return items;
+	}
+	
+}
