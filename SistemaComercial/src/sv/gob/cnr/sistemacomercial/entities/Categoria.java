51027bd9d9a71ca89c27d831b8dff7278ccbf1f0
juan.diaz <juan.diaz@DTI-W7JDIAZ.cnr.gob.sv>
2018-10-24 15:55:28 -0600

    Se agregó clase Categoria

---
+++ SistemaComercial/src/sv/gob/cnr/sistemacomercial/entities/Categoria.java
@@ -0,0 +1,11 @@
+package sv.gob.cnr.sistemacomercial.entities;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
