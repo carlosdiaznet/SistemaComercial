b682e95da0f817cac692ebf70ba6b626099a9a23
Carlos Diaz <carlosdiaz.app@gmail.com>
2018-12-10 05:19:35 -0600

    Lista categoria se completa con DB, sigue separar el EntityM de Beans vt12.1

--- SistemaComercial/src/sv/gob/cnr/sistemacomercial/mbeans/RegistroProductoMB.java
+++ SistemaComercial/src/sv/gob/cnr/sistemacomercial/mbeans/RegistroProductoMB.java
@@ -1,10 +1,15 @@
 package sv.gob.cnr.sistemacomercial.mbeans;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import sv.gob.cnr.sistemacomercial.entities.Categoria;
 import sv.gob.cnr.sistemacomercial.entities.Producto;
 
 @ManagedBean(name = "registroProductoMB")
@@ -15,8 +20,19 @@ public class RegistroProductoMB implements Serializable {
 	
 	private Producto producto;
 	
+	private List<Categoria> listCategoria;
+	
 	public RegistroProductoMB(){
 		producto = new Producto();
+	}
+	
+	public void inicializar(){
+		System.out.println("Inicializando lista...");
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("siscomPU");
+		EntityManager manager = factory.createEntityManager();
+		
+		listCategoria = manager.createQuery("select c from Categoria c", Categoria.class).getResultList();
+		manager.close();
 	}
 	
 	public void guardar(){
@@ -29,6 +45,10 @@ public class RegistroProductoMB implements Serializable {
 
 	public void setProducto(Producto producto) {
 		this.producto = producto;
+	}
+
+	public List<Categoria> getListCategoria() {
+		return listCategoria;
 	}
 	
 	
