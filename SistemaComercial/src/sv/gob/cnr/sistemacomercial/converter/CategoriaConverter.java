e7b80b3aeec590b82de6859acb7453ddbf7ce965
Carlos Diaz <carlosdiaz.app@gmail.com>
2019-01-14 05:21:29 -0600

    cambios hoy

--- SistemaComercial/src/sv/gob/cnr/sistemacomercial/converter/CategoriaConverter.java
+++ SistemaComercial/src/sv/gob/cnr/sistemacomercial/converter/CategoriaConverter.java
@@ -6,7 +6,7 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
-import javax.inject.Inject;
+//import javax.inject.Inject;
 
 //import sv.gob.cnr.sistemacomercial.cdi.CDIServiceLocator;
 import sv.gob.cnr.sistemacomercial.entities.Categoria;
@@ -16,31 +16,42 @@ import sv.gob.cnr.sistemacomercial.repositories.CategoriaRepository;
 @FacesConverter("categoriaConverter")
 public class CategoriaConverter implements Converter {
 	
+	private Categoria objCat;
+	private CategoriaRepository repCat;
 
+	public CategoriaConverter() {
+		System.out.println("soy el constructor");
+		Categoria objCat = new Categoria();
+		CategoriaRepository repCat = new CategoriaRepository();
+		System.out.println("fin del constructor");
+	}
+	
+	public void asignacion(Long id){
+		System.out.println("funcion llamada...");
+	}
 
 	@Override
-	public Object getAsObject(FacesContext context, UIComponent component, String value) {
-		Categoria objCategoria = null;
-		
-		
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {	
 		if(value != null){
 			Long id = null;
 			try {
 				System.out.println("es de tipo String: " + value);
 				System.out.println(value);
+				System.out.println(value);
 				id = Long.valueOf(value);
+				System.out.println("Es de tipo long: " + id);
+				//this.objCat = this.repCat.byId(id);
+				//CategoriaRepository categoriaId = new CategoriaRepository();
+				System.out.println("paso la asignacion");
+				//objCategoria = categoriaId.byId(id);
 				
-				CategoriaRepository categoriaId = new CategoriaRepository();
-				
-				objCategoria = categoriaId.byId(id);
-				
-				System.out.println("Asigna el ID" + objCategoria);
+				//System.out.println("Asigna el ID" + objCategoria);
 			} catch (NumberFormatException e) {
 				// TODO: handle exception
 			}
 			
 		}
-		return objCategoria;
+		return null;
 	}
 
 	@Override
