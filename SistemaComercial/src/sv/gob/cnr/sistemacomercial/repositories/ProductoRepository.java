947674932b79a210edd118179cfb7db2fd3fbbbf
Carlos Diaz <carlosdiaz.app@gmail.com>
2019-02-19 05:37:19 -0600

    Buscando crear el filtro con consulta jpql

--- SistemaComercial/src/sv/gob/cnr/sistemacomercial/repositories/ProductoRepository.java
+++ SistemaComercial/src/sv/gob/cnr/sistemacomercial/repositories/ProductoRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.Query;
 
 import sv.gob.cnr.sistemacomercial.entities.Producto;
 import sv.gob.cnr.sistemacomercial.filter.ProductoFilter;
@@ -21,8 +22,18 @@ public class ProductoRepository implements Serializable {
 		return manager.createQuery("select p from Producto p", Producto.class).getResultList();
 	}
 	
+	@SuppressWarnings("unchecked")
 	public List<Producto> filtrados(ProductoFilter filtro){
-		return null;
+		if(filtro.getSku() != null && filtro.getNombre() != null){
+			Query query = manager.createQuery("SELECT p FROM Producto p WHERE p.sku LIKE %:sku% OR p.nombre LIKE %:nombre%", Producto.class);
+				query.setParameter("sku", filtro.getSku());
+				query.setParameter("nombre", filtro.getNombre());
+				return query.getResultList();
+		} else {
+			return null;
+		}
+		
+		
 	}
 	
 	public Producto porSku(String sku){
